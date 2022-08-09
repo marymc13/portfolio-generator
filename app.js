dd6d81764f3e864584cd5839f24c7236e700c8c7
Mary Paul <marymc135@hotmail.com>
2022-08-09 19:30:33 -0400

    creating promises

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -126,13 +125,39 @@ if (!portfolioData.projects) {
       .then(promptProject)
       .then(portfolioData => {
         
- const pageHTML = generatePage(portfolioData);
+     return generatePage(portfolioData);
+      })
 
-fs.writeFile('./index.html', pageHTML, err => {
-    if(err) throw err;
+      .then(pageHTML => {
+        return writeFile(pageHTML);
+      })
+      .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+      })
 
-    console.log('Portfolio complete!  Check out index.html to see the output!');
-});
+      .then(copyFileResponse => {
+        console.log(copyFileResponse);
+      })
+      .catch(err => {
+        console.log(err);
       });
+
+fs.writeFile('./dist/index.html', pageHTML, err => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    console.log('Page created!  Check out index.html in this directory to see the output!');
+
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        console.log(err);
+        return;
+    }  
+    console.log('Style sheet copied successfully!');  
+});
+  });
       
    
